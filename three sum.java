71c51b8b083e6886dd782dc059ccf29fca7441a3
Shivani Kushwah <kushwahshivani617@gmail.com>
2026-04-23 21:03:14 +0530

    Create three sum.java

---
+++ three sum.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class ThreeSumSimple {
+    public static void main(String[] args) {
+        int[] arr = {-1, 0, 1, 2, -1, -4};
+
+        Arrays.sort(arr); // Step 1: sort array
+
+        for (int i = 0; i < arr.length - 2; i++) {
+
+            int left = i + 1;
+            int right = arr.length - 1;
+
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+
+                if (sum == 0) {
+                    System.out.println(arr[i] + " " + arr[left] + " " + arr[right]);
+                    left++;
+                    right--;
+                } 
+                else if (sum < 0) {
+                    left++; // increase sum
+                } 
+                else {
+                    right--; // decrease sum
+                }
+            }
+        }
+    }
+}
